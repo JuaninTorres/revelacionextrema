49cce6a8b36f61ead5f5eae11790989b0d3a54fe
Juan Torres <juanin.torres@gmail.com>
2014-09-29 15:11:56 -0300

    Mejornado el HOME

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
-    <link href='http://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}"/>
     <title>Document</title>
 </head>
@@ -19,8 +19,26 @@
                                                                                                                                                                                     </div>
         <div id="der">
             <header>
-                @yield('header')
-                @yield('navegacion-principal')
+                <div class="titulo">
+                    <picture><img src="{{ asset('images/header_titulo.png') }}" alt=""/></picture>
+                </div>
+                <div class="social">
+                    <ul>
+                    <li><a href="#" class="social-twitter">Twitter</a></li>
+                    <li><a href="https://www.facebook.com/RevelacionExtrema" class="social-facebook">Facebook</a></li>
+                    <li><a href="#" class="social-youtube">YouTube</a></li>
+                    </ul>
+                </div>
+                <nav class="menu-principal">
+                    <ul>
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/staff">Staff</a></li>
+                        <li><a href="/horarios">Horarios</a></li>
+                        <li><a href="/tv">TV</a></li>
+                        <li><a href="/evntos">Eventos</a></li>
+                        <li><a href="/contacto">Contacto</a></li>
+                    </ul>
+                </nav>
             </header>
 
             @yield('contenido')
